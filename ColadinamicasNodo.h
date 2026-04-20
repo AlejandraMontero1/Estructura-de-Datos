ef269e414bab154259722237308b207a72cb0310
AlejandraMontero1 <a2025113919@estudiantes.upsa.edu.bo>
2026-04-20 13:25:49 -0400

    Create ColadinamicasNodo.h

---
+++ ColadinamicasNodo.h
@@ -0,0 +1,14 @@
+Nodo.h
+//clase Nodo 
+
+
+#pragma once 
+#include "Dato.h"
+class Nodo { public: 
+    Dato dato; 
+    Nodo *puntero; 
+     
+    Nodo(){ //Constructor de la clase
+        puntero = NULL; 
+    }
+};
